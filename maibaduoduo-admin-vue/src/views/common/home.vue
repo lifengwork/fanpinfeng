6eeb099585bcd32efe4ed4ccf5ce7c53d1a60cf5
lifengwork <lifengwork@yeah.net>
2023-04-22 14:53:55 +0800

    saas业务端页面初始化

---
+++ maibaduoduo-admin-vue/src/views/common/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mod-home">
+    <h3>项目介绍</h3>
+    <ul>
+      <li>欢迎您的访问。</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
