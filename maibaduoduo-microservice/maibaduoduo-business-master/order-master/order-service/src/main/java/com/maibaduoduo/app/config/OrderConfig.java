b6df0fdf2abf5311acbe3dfa6943f245bd0fc09e
lifengwork <lifengwork@yeah.net>
2023-04-24 21:12:31 +0800

    修改服务请求配置文件

--- maibaduoduo-microservice/maibaduoduo-business-master/order-master/order-service/src/main/java/com/maibaduoduo/app/config/OrderConfig.java
+++ maibaduoduo-microservice/maibaduoduo-business-master/order-master/order-service/src/main/java/com/maibaduoduo/app/config/OrderConfig.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
--- maibaduoduo-microservice/maibaduoduo-master/maibaduoduo-configuration/src/main/java/com/maibaduoduo/configuration/config/FeignRequestInterceptor.java
+++ maibaduoduo-microservice/maibaduoduo-master/maibaduoduo-configuration/src/main/java/com/maibaduoduo/configuration/config/FeignRequestInterceptor.java
@@ -2,10 +2,12 @@ package com.maibaduoduo.configuration.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Administrator on 2019/11/17 0017.
  */
+@Component
 public class FeignRequestInterceptor  implements RequestInterceptor {
     /**
      * 设置请求相关信息
