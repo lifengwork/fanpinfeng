d571667b73f20ba99c060434d20af7683f2f5ccc
lifengwork <lifengwork@yeah.net>
2023-10-20 16:10:55 +0800

    增加库存服等

---
+++ maibaduoduo-microservice/maibaduoduo-business-master/inventory-master/inventory-core/src/main/java/com/maibaduoduo/inventory/event/Event.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2019-2023 SAAS开源 All rights reserved.
+ *
+ * SAAS系统设计研发交流
+ *
+ * https://www.maibaduoduo.com
+ */
+package com.maibaduoduo.inventory.event;
+
+import com.maibaduoduo.configuration.utils.IdGen;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * 基础事件类
+ */
+public class Event extends ApplicationEvent {
+    public Event(Object source) {
+        super(source);
+    }
+
+    public String getEventId() {
+        return IdGen.SnowFlake();
+    }
+}
